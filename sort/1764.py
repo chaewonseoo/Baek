0fa9c9c06aac8ad8894ee33424447851cf60bb39
chaewss <chaewseo12@gmail.com>
2021-08-10 04:50:28 +0900

    BOJ

---
+++ sort/1764.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == '__main__':
+    n, m = map(int, sys.stdin.readline().split())
+    listen = set([sys.stdin.readline()[:-1] for _ in range(n)])
+    see = set([sys.stdin.readline()[:-1] for _ in range(m)])
+
+    result = sorted(list(listen & see))
+    print(len(result))
+    [print(i) for i in result]
